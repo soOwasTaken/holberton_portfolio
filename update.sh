800f1efa1fd02d845fc8dd60d2b9102ace918351
soO <444deph12@gmail.com>
2023-06-16 06:28:13 -0700

    update

--- update.sh
+++ update.sh
@@ -2,9 +2,12 @@
 
 cd game-2d
 git pull
+npm run build
 cd ..
 cd game-3d
 git pull
+parcel build src/index.html --public-url ./
 cd ..
 
-echo "Repositories Updated Properly"
+echo "Repositories & Builds Updated Properly"
+#!/bin/bash
