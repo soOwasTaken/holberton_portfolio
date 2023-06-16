32afefa30ce249a6e01866cffdb13a06e098f34f
soO <444deph12@gmail.com>
2023-06-16 06:36:50 -0700

    updated update.sh

--- update.sh
+++ update.sh
@@ -6,8 +6,12 @@ npm run build
 cd ..
 cd game-3d
 git pull
-parcel build src/index.html --public-url ./
+
 cd ..
 
-echo "Repositories & Builds Updated Properly"
-#!/bin/bash
+echo "Repositories & build sucessfully Updated Properly"
+node server.js
+
+echo "vlaaaaaaa le serveur il est lancer en balle et tout là"
+echo "http://localhost:3000/"
+echo "http://localhost:3000/game-2d"
