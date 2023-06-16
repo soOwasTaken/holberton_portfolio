6d65c04fd5fbf66b12f011f358fadb8119c19ad4
soO <444deph12@gmail.com>
2023-06-16 02:25:30 -0700

    add of setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/soOwasTaken/GQ2D game-2d
+mv game-2d game-2d
+git clone https://github.com/rChrisb/Galaxy_3D game-3d
+mv game-3d game-3d
+
+echo "Repositories cloned and folders renamed successfully!"
