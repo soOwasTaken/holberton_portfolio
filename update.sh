c68f27b0598fe7349bf3300fae864146ce93fac9
rChrisb <5767@holbertonstudents.com>
2023-06-25 18:42:14 +0200

    add scenes in planet5 and planet2

--- update.sh
+++ update.sh
@@ -7,6 +7,8 @@ cd ..
 cd game-3d
 git pull
 parcel build src/index.html --public-url ./
+parcel build src/pinkplanet.html --public-url ./
+parcel build src/redplanet.html --public-url ./
 cd ..
 
 echo "Repositories & build sucessfully Updated Properly"
